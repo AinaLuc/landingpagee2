9c7ceba39da60ef3de0a1db199129659269e2c7c
AinaLuc <luaina.mada@gmail.com>
2024-04-15 14:45:06 +0400

    email revamp

--- src/components/VideoPage.vue
+++ src/components/VideoPage.vue
@@ -36,7 +36,7 @@ export default {
   methods: {
     redirectToOrderPage() {
       // Redirect to the specified URL when the button is clicked
-      window.location.href = 'https://calendly.com/tanamteche2/30min';
+      window.location.href = 'https://calendly.com/tanamteche2/e2-visa';
     },
 
     loadYouTubePlayer() {
